cc995256acf476abf207df907346af058ff8ff85
VADUNG <vad24042002@gmail.com>
2024-04-16 22:36:57 +0700

    push api

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL, // cho phép URL ở client lấy được data ở server
+    methods: ["POST", "GET", "PUT", "DELETE"], // giới hạn 4 method
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.listen(3001, () => {
+  console.log("Server is running on port 3000!");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
